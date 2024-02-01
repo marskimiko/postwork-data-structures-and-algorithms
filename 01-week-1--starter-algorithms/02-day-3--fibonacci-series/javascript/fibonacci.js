def6728459ad86400909026b544525f07b8dbd13
Marisa Saunders <marisaksaunders@gmail.com>
2024-02-01 13:13:09 -0500

    preferred solution

--- 01-week-1--starter-algorithms/02-day-3--fibonacci-series/javascript/fibonacci.js
+++ 01-week-1--starter-algorithms/02-day-3--fibonacci-series/javascript/fibonacci.js
@@ -1,17 +1,31 @@
+// function fibonacci(num) {
+//   // type your code here
+//   if (num === 0) return 0;
+//   let spot1 = 0;
+//   let spot2 = 1;
+//   let current;
+
+//   for (i = 2; i <= num; i++) {
+//     current = spot1 + spot2;
+//     spot1 = spot2;
+//     spot2 = current;
+//   }
+
+//   return spot2;
+// }
+
 function fibonacci(num) {
-  // type your code here
   if (num === 0) return 0;
-  let spot1 = 0;
-  let spot2 = 1;
-  let current;
+  
+  const arr = [0, 1];
+  let a, b;
 
-  for (i = 2; i <= num; i++) {
-    current = spot1 + spot2;
-    spot1 = spot2;
-    spot2 = current;
+  for(let i = 2; i <= num; i++) {
+    a = arr[i - 1];
+    b = arr[i - 2];
+    arr.push(a + b);
   }
-
-  return spot2;
+  return parseInt(arr.slice(-1));
 }
 
 if (require.main === module) {
